904388c22d098c5141c8a1bf7b98767147f76caf
korn33 <korn33.mail@gmail.com>
2021-02-19 22:26:18 +0300

    header, navbar, profile grid

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Header = () => {
+    return <header className='header'>
+        <img className='App-logo' src='logo192.png'/>
+    </header>
+};
+
+export default Header
---
+++ src/components/Profile.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Profile = () => {
+    return <div className='content'>
+        <div className='profile-header'>
+        </div>
+        <div>
+            ava+description
+        </div>
+        <div>
+            my posts
+            <div>
+                new post
+                <div>post 1</div>
+                <div>post 2</div>
+                <div>post 3</div>
+            </div>
+        </div>
+    </div>
+};
+
+export default Profile
