dd7da9e8f339d97b328308506a2226b5360575ed
korn33 <korn33.mail@gmail.com>
2021-03-31 23:05:55 +0300

    add consol.log form with formik

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,9 +1,50 @@
 import React from "react";
 import s from './Login.module.css';
+import {ErrorMessage, Field, Form, Formik, useFormik} from "formik";
+
+const SignupForm = () => {
+    return (
+        <Formik
+            initialValues={{
+                login: '',
+                password: '',
+                rememberMe: false,
+            }}
+            onSubmit={values => {
+                console.log(values);
+            }}
+        >
+            <Form>
+                <div>
+                    <label htmlFor="login">login</label>
+                    <Field name="login" type="text"/>
+                    <ErrorMessage name="login"/>
+                </div>
+                <div>
+                    <label htmlFor="password">password</label>
+                    <Field name="password" type="text"/>
+                    <ErrorMessage name="password"/>
+                </div>
+                <div>
+                    <label htmlFor="rememberMe">rememberMe</label>
+                    <Field name="rememberMe" type="checkbox"/>
+                    <ErrorMessage name="rememberMe"/>
+                </div>
+                <button type="submit">Submit</button>
+            </Form>
+        </Formik>
+    );
+};
 
 const Login = (props) => {
-    return (
-        <h1 className={s.message}>Need <a href={'https://social-network.samuraijs.com'} target="_blank">sign in</a></h1>
+    const test = () => {
+        console.log('test');
+    };
+    return (<>
+            <h1 className={s.message}>Need <a href={'https://social-network.samuraijs.com'} target="_blank">sign
+                in</a> or:</h1>
+            <SignupForm onSubmit={test}/>
+        </>
     )
 };
 
